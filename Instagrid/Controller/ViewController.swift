5f0a96d6aea86a0ade437028d7b28100a6595b51
sadtuga <tuga@vivaldi.net>
2018-12-18 21:31:17 +0100

    Creation of the interface (responsive portrait / landscape)

---
+++ Instagrid/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Marques Lucas on 18/12/2018.
+//  Copyright © 2018 Marques Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
